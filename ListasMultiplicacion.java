d665a2a54066e43a59793295661c3c8ce730bc9e
Rick <ricardomera300@gmail.com>
2025-02-15 17:17:08 -0500

    Update ListasMultiplicacion.java

--- ListasMultiplicacion.java
+++ ListasMultiplicacion.java
@@ -13,23 +13,23 @@ public class ListasMultiplicacion {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Ingrese la cantidad de números para la primera lista: ");
+        System.out.print("ingrese la cantidad de numeros para la primera lista: ");
         int n = scanner.nextInt();
         List<Integer> lista1 = new ArrayList<>();
-        System.out.println("Ingrese los números:");
+        System.out.println("ingrese los numeros:");
         for (int i = 0; i < n; i++) {
             lista1.add(scanner.nextInt());
         }
 
-        System.out.print("Ingrese la cantidad de números para la segunda lista: ");
+        System.out.print("ingrese la cantidad de numeros para la segunda lista: ");
         int m = scanner.nextInt();
         List<Integer> lista2 = new ArrayList<>();
-        System.out.println("Ingrese los números:");
+        System.out.println("ingrese los numeros:");
         for (int i = 0; i < m; i++) {
             lista2.add(scanner.nextInt());
         }
 
-        System.out.println("Multiplicación de listas: " + listasMultiplicacion(lista1, lista2));
+        System.out.println("multiplicación de listas: " + listasMultiplicacion(lista1, lista2));
         scanner.close();
     }
 }
